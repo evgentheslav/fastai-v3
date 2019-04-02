d5191660f9032681ea8ca95a19a6bfe87ad0b52e
evgenii <evgenii@supereffective.com>
2019-04-02 21:02:06 +0800

    Model updated

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/gv5s7i1jb137kc8/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['monet', 'manet']
 path = Path(__file__).parent
 
 app = Starlette()
